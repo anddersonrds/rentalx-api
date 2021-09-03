ff227f7700fbafc94f83b3bebcf00d74585a9165
Anderson Rodrigues <anddersonrds@gmail.com>
2021-09-03 01:07:05 -0300

    Add Categories

---
+++ src/routes/categories.routes.ts
@@ -0,0 +1,16 @@
+import { Router } from 'express'
+
+import { CategoryRepository } from '../repositories/CatgoriesRepository'
+
+const categoriesRoutes = Router()
+const categoriesRepository = new CategoryRepository()
+
+categoriesRoutes.post('/', (request, response) => {
+  const { name, description } = request.body
+
+  categoriesRepository.create({ name, description })
+
+  return response.status(201).send()
+})
+
+export { categoriesRoutes }
